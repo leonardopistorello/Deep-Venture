ecb7197745a6397a4a6423251896392d0cfb82db
Giorgia9 <67117061+Giorgia9@users.noreply.github.com>
2021-09-22 14:43:34 +0200

    HungerBar UI and Script, Seaweed Script

---
+++ Assets/RW/Scripts/Tapping mode/ProgressBarController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ProgressBarController : MonoBehaviour
+{
+    public static ProgressBarController Instance;
+    public Image ProgressBar;
+    
+    /* crea un'unica istanza dell'oggetto ProgressBarController */
+    private void Awake()
+    {   
+        if(Instance == null) {
+            DontDestroyOnLoad(gameObject);
+            Instance = this; 
+
+        }
+        else if (Instance !=this) {
+            Destroy(gameObject);
+        }
+    }
+
+  
+
+    /* la barra viene svuotata man mano che scorre il tempo */
+    void Update()
+    {
+        if(ProgressBar.fillAmount > 0f) { 
+           ProgressBar.fillAmount -= 0.001f; //la barra diminuisce di livello
+           if(ProgressBar.fillAmount <= 0.5f) //quando arriva a metà
+              ProgressBar.color = Color.red;  //cambia il colore della barra
+        }
+
+        
+    }
+    /* aumenta di poco il livello della barra */
+    public void FillProgressBar() {
+        ProgressBar.fillAmount += 0.2f;
+        /* la barra torna verde se il livello supera la metà */
+        if(ProgressBar.fillAmount >= 0.5f) {
+          Color myGreen = new Color32(51,212,53,181);
+          ProgressBar.color = myGreen;
+        }
+    }
+
+
+}
--- Assets/RW/Scripts/Tapping mode/SeaweedController.cs
+++ Assets/RW/Scripts/Tapping mode/SeaweedController.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class SeaweedController : MonoBehaviour
 {
+    ProgressBarController bar;
+   void Start() {
+        bar = ProgressBarController.Instance;
+   }
     void OnMouseDown() {
-        
+        bar.FillProgressBar();
+        Kill();
+    }
+
+    private void Kill() {
+       this.gameObject.SetActive(false);
     }
 }
