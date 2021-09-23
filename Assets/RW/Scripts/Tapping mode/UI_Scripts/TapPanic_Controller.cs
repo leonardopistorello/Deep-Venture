26521f3b67fd155c1b0ae819b570cdd1c7e4e3ea
Giorgia9 <67117061+Giorgia9@users.noreply.github.com>
2021-09-23 02:15:50 +0200

    Scene Load Scripts

---
+++ Assets/RW/Scripts/Tapping mode/UI_Scripts/TapPanic_Controller.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class TapPanic_Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void StartTapPanic()
+    {
+        SceneManager.LoadScene("TapPanic");
+    }
+    
+
+    
+}
